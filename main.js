d8d88f40ce3de9d19728c9a2e4dd1d07e50fd8f6
Pachi <pachicodes@gmail.com>
2019-11-05 15:45:00 -0500

    final detail

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-window.addEventListener('keydown', function playSound(e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
-});
+}
 
 function removeTransition(e) {
   if(e.propertyName !== 'transform') return;
@@ -14,3 +14,4 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
